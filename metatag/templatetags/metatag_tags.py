04e258a176c7cbbe4a2f4ebf49bc7b008f18449f
ivaggione <ivaggione@gmail.com>
2018-06-21 14:42:47 +0000

    Django 1.11.4 support

--- metatag/templatetags/metatag_tags.py
+++ metatag/templatetags/metatag_tags.py
@@ -9,27 +9,29 @@ from metatag.resolve_to_name_snippet import resolve_to_name
 
 register = template.Library()
 
+
 class URLMetatagsNode(template.Node):
     def render(self, context):
         request = context['request']
         try:
             url = resolve_to_name(request.path)
-            meta = URLMetatags.objects.get(Q(url=url)|Q(url=request.path))
+            meta = URLMetatags.objects.get(Q(url=url) | Q(url=request.path))
 
         except URLMetatags.DoesNotExist:
             return ''
 
         meta_dict = {}
-
-        for key in URLMetatags._meta.get_all_field_names():
-            att = getattr(meta,key,None)
+        all_names = [f.name for f in URLMetatags._meta.get_fields()]
+        for key in all_names:
+            att = getattr(meta, key, None)
             if att:
                 t = Template(att)
                 meta_dict[key] = t.render(context)
 
         context['metatag'] = meta_dict
-        return '' 
-    
+        return ''
+
+
 @register.tag()
-def metatag_populate(parser,token):
+def metatag_populate(parser, token):
     return URLMetatagsNode()
