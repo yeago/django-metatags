e4c139f18c3f350bfeb90b4dc27d1e615709da1e
steve yeago <subsume@gmail.com>
2020-01-29 01:44:50 -0500

    Update special_class.py

--- metatag/special_class.py
+++ metatag/special_class.py
@@ -1,10 +1,10 @@
 list_of_subclasses = []
 
 class MetaMetatag(type):
-	def __init__(cls, name, bases, dict):
-		super(MetaMetatag,cls).__init__(name,bases,dict)
-		if cls.__name__ != "Metatag":
-			list_of_subclasses.append(cls)
+    def __init__(cls, name, bases, dict):
+        super(MetaMetatag,cls).__init__(name,bases,dict)
+        if cls.__name__ != "Metatag":
+            list_of_subclasses.append(cls)
 
 class Metatag(type):
-	__metaclass__ = MetaMetatag
+    __metaclass__ = MetaMetatag
